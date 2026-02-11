e99d7ed7fa54aa3d7780d80fc7142e661feebc9f
VajiraPrabuddhaka <vajiraprabuddhaka@gmail.com>
2026-02-11 11:04:45 +0530

    Test commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-console.log('Doclet app is starting......!')
+console.log('Doclet app is starting...')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
